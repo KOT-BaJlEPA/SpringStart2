baa25b9a3011317cfaadc32f40be831397679d5b
KOT-BaJlEPA <regulusblack1991@gmail.com>
2024-07-25 22:26:51 +0300

    bean scope

--- src/main/java/spring/_2BeanScope/_1singletone/Test2.java
+++ src/main/java/spring/_2BeanScope/_1singletone/Test2.java
@@ -7,8 +7,8 @@ public class Test2 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContextBeanScope.xml");
         System.out.println(" the object Flat and BathRoom is created regardless of we call context.getBean() or not");
 
-        Flat flat1 = (Flat) context.getBean("myFlat");
-        Flat flat2 = (Flat) context.getBean("myFlat");
+        Flat flat1 = (Flat) context.getBean("myFlat", Flat.class);
+        Flat flat2 = (Flat) context.getBean("myFlat", Flat.class);
 
         System.out.println();
         System.out.println( "   flat1   " + flat1);
@@ -22,6 +22,8 @@ public class Test2 {
         System.out.println( "   flat1   " + flat1);
         System.out.println( "       flat2   " + flat2);
         System.out.println(" it means that context return only one object");
+        System.out.println(context.getBean("myFlat", Flat.class) == context.getBean("myFlat", Flat.class));
+        //true
         context.close();
 
 
